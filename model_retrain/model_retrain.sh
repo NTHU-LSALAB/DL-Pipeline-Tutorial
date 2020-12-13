0aec7bf6d3f4ddeafcdc5e4f324d4a9e6e4038c9
lsalab-member <75566591+lsalab-member@users.noreply.github.com>
2020-12-13 23:10:50 +0800

    Update model_retrain.sh

--- model_retrain/model_retrain.sh
+++ model_retrain/model_retrain.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mv ~/photos/dog/* ~/model_retrain/data/train/dog/
-mv ~/photos/cat/* ~/model_retrain/data/train/cat/
+mv ~/photos/dog/* $HOME/DL-Pipeline-Tutorial/model_retrain/data/train/dog/
+mv ~/photos/cat/* $HOME/DL-Pipeline-Tutorial/model_retrain/data/train/cat/
 
-version=$(ls -1 ~/saved_model/input_models | wc -l) # The model gonna be trained is the verison $version
+version=$(ls -1 $HOME/DL-Pipeline-Tutorial/saved_model/input_models | wc -l) # The model gonna be trained is the verison $version
 # echo Model version is: $version
 python3 retrain.py data model | tee output_v$version
 sleep 5
@@ -11,10 +11,10 @@ accuracy=$(cat output_v$version | tail -n 3 | head -n 1 | awk '{print $11}')
 # echo Accuracy is: $accuracy
 mv output_v$version output_v$version\_$accuracy
 
-saved_model_input="~/saved_model/input_models/top_model_weights_$version.h5"
-saved_model_output="~/saved_model/x_test/$version"
-cp ~/model_retrain/model/top_model_weights.h5 $saved_model_input
-python3 ~/saved_model/export_saved_model.py $saved_model_input $saved_model_output
+saved_model_input="$HOME/DL-Pipeline-Tutorial/saved_model/input_models/top_model_weights_$version.h5"
+saved_model_output="$HOME/DL-Pipeline-Tutorial/saved_model/x_test/$version"
+cp $HOME/DL-Pipeline-Tutorial/model_retrain/model/top_model_weights.h5 $saved_model_input
+python3 $HOME/DL-Pipeline-Tutorial/saved_model/export_saved_model.py $saved_model_input $saved_model_output
 
 sleep 10
 echo "Done!!! This is model version $version"
