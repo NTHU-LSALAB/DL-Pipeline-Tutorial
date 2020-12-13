cb7d66b96d9778fa06ad56e4eaef55c8220a674a
lsalab-member <75566591+lsalab-member@users.noreply.github.com>
2020-12-14 01:41:12 +0800

    Update model_retrain.sh

--- model_retrain/model_retrain.sh
+++ model_retrain/model_retrain.sh
@@ -13,6 +13,8 @@ mv output_v$version output_v$version\_$accuracy
 saved_model_input="$HOME/DL-Pipeline-Tutorial/saved_model/input_models/top_model_weights_$version.h5"
 saved_model_output="$HOME/DL-Pipeline-Tutorial/saved_model/x_test/$version"
 cp $HOME/DL-Pipeline-Tutorial/model_retrain/model/top_model_weights.h5 $saved_model_input
+
+#exoprt savedmodel from .h5 file
 python3 $HOME/DL-Pipeline-Tutorial/saved_model/export_saved_model.py $saved_model_input $saved_model_output
 
 sleep 10
