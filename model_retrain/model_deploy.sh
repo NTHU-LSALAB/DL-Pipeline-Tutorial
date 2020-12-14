a8ebb5960a6b69e8e90886fc11ca3367551a4014
lsalab-member <75566591+lsalab-member@users.noreply.github.com>
2020-12-14 11:57:48 +0800

    Update model_deploy.sh

--- model_retrain/model_deploy.sh
+++ model_retrain/model_deploy.sh
@@ -6,7 +6,7 @@ if [[ -z $1 ]]; then
 fi
 
 version=$1
-
+mkdir -p ~/tf-serving/x_test
 cp -r ~/saved_model/x_test/$version ~/tf-serving/x_test/$version
 sed -i "s/rest_api_port=850[0-9]/rest_api_port=850$version/" ~/tf-serving/Dockerfile
 docker build ~/tf-serving -t cwhuang29/tf-serving:v$version
