c6900430382a0a52dd6b4cc393cec996a11b2707
lsalab-member <75566591+lsalab-member@users.noreply.github.com>
2020-12-14 21:04:17 +0800

    Update model_deploy.sh

--- model_retrain/model_deploy.sh
+++ model_retrain/model_deploy.sh
@@ -6,23 +6,23 @@ if [[ -z $1 ]]; then
 fi
 
 version=$1
-mkdir -p ~/tf-serving/x_test
-cp -r ~/saved_model/x_test/$version ~/tf-serving/x_test/$version
-sed -i "s/rest_api_port=850[0-9]/rest_api_port=850$version/" ~/tf-serving/Dockerfile
-docker build ~/tf-serving -t cwhuang29/tf-serving:v$version
+mkdir -p $HOME/DL-Pipeline-Tutorial/tf-serving/x_test
+cp -r $HOME/DL-Pipeline-Tutorial/saved_model/x_test/$version $HOME/DL-Pipeline-Tutorial/tf-serving/x_test/$version
+sed -i "s/rest_api_port=850[0-9]/rest_api_port=850$version/" $HOME/DL-Pipeline-Tutorial/tf-serving/Dockerfile
+docker build $HOME/DL-Pipeline-Tutorial/tf-serving -t cwhuang29/tf-serving:v$version
 docker push cwhuang29/tf-serving:v$version
 
 # tfserving.yml -> tfserving_v#.yml
-cp ~/keadm-v1.4.0-linux-amd64/keadm/tfserving.yml ~/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
+cp $HOME/DL-Pipeline-Tutorial/keadm-v1.4.0-linux-amd64/keadm/tfserving.yml $HOME/DL-Pipeline-Tutorial/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
 # metadata.name
-sed -i "s/tfserving-deployment.*/tfserving-deployment-v$version/" ~/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
+sed -i "s/tfserving-deployment.*/tfserving-deployment-v$version/" $HOME/DL-Pipeline-Tutorial/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
 # spec.template.spec.containers.image
-sed -i "s/tf-serving:v[0-9]\+/tf-serving:v$version/" ~/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
+sed -i "s/tf-serving:v[0-9]\+/tf-serving:v$version/" $HOME/DL-Pipeline-Tutorial/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
 # spec.template.spec.containers.ports.containerPort
-sed -i "s/containerPort: 850[0-9]\+/containerPort: 850$version/" ~/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
+sed -i "s/containerPort: 850[0-9]\+/containerPort: 850$version/" $HOME/DL-Pipeline-Tutorial/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
 # spec.template.spec.containers.ports.hostPort
-sed -i "s/hostPort: 850[0-9]\+/hostPort: 850$version/" ~/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
-kubectl apply -f ~/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
+sed -i "s/hostPort: 850[0-9]\+/hostPort: 850$version/" $HOME/DL-Pipeline-Tutorial/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
+kubectl apply -f $HOME/DL-Pipeline-Tutorial/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml
 #if [[ -z ~/keadm-v1.4.0-linux-amd64/keadm/tfserving_v$version.yml ]]; then
 #        echo "File not found !!!!!!!!!!!!";
 #fi
